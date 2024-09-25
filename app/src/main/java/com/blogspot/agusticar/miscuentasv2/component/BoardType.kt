5133e6fcea7f697fa2e0202e795d8881761c1ecf
Agustin Carnerero Peña <agusticar@gmail.com>
2024-09-25 13:18:32 +0200

    Inicio del proyecto MisCuentas en Jetpack Compose

---
+++ app/src/main/java/com/blogspot/agusticar/miscuentasv2/component/BoardType.kt
@@ -0,0 +1,4 @@
+package com.blogspot.agusticar.miscuentasv2.component
+
+enum class BoardType {EMAIL,PASSWORD,PHONE,NUMBER,TEXT,URI,DECIMAL
+}
---
+++ app/src/main/java/com/blogspot/agusticar/miscuentasv2/component/TextFieldComponent.kt
@@ -0,0 +1,102 @@
+package com.blogspot.agusticar.miscuentasv2.component
+
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.blogspot.agusticar.miscuentasv2.R
+
+
+@Composable
+
+fun TextFieldComponent(
+    modifier: Modifier,
+    label: String,
+    inputText: String,
+    onTextChange: (String) -> Unit,
+    type: BoardType,
+    passWordVisible: Boolean=false, // Boolean to determine if password should be visible or not
+) {
+    var visiblePasswordIcon by rememberSaveable { mutableStateOf(false) }
+    TextField(
+        value = inputText,
+        onValueChange = {
+            // Lógica para cambiar "a" por "A"
+            //onTextChange(if (it.contains("a")) it.replace("a", "A") else it)
+            onTextChange(it)
+            //equivale a  newValue -> onTextChange(newValue)
+        },
+        label = { Text(text = label) },
+        shape = RoundedCornerShape(10.dp),
+        maxLines = 1,
+        singleLine = true, // Limita la altura de la caja de entrada a una sola línea
+
+        modifier = modifier
+            .wrapContentHeight() // Altura ajustable
+            .padding(16.dp), keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = getBoardType(type) // Set the keyboard type based on the input parameter
+        ),
+        visualTransformation = if (passWordVisible && !visiblePasswordIcon) PasswordVisualTransformation() else VisualTransformation.None, // Change display based on boolean
+
+        colors = TextFieldDefaults.colors(
+            contentColorFor(backgroundColor = Color.Transparent),
+            focusedIndicatorColor = Color.Transparent,  // Sin borde cuando está enfocado
+            unfocusedIndicatorColor = Color.Transparent  // Sin borde cuando no está enfocado
+
+        ),
+        trailingIcon = {
+            if(type==BoardType.PASSWORD){
+                if(visiblePasswordIcon) {
+                    Icon(painter = painterResource (id=R.drawable.visibility), contentDescription ="Contraseña visible" )
+
+                }else {
+                    Icon(
+                        painter = painterResource(id = R.drawable.visibility_off),
+                        contentDescription = "Contraseña no visible"
+                    )
+                }
+                IconButton(onClick = {visiblePasswordIcon=!visiblePasswordIcon}) {
+                }
+            }
+        }
+    )
+}
+
+private fun getBoardType(type: BoardType): KeyboardType {
+    return when (type) {
+        BoardType.EMAIL -> KeyboardType.Email
+        BoardType.NUMBER -> KeyboardType.Number
+        BoardType.PASSWORD -> KeyboardType.Password
+        BoardType.PHONE -> KeyboardType.Phone
+        BoardType.TEXT-> KeyboardType.Text
+        BoardType.URI -> KeyboardType.Uri
+        BoardType.DECIMAL -> KeyboardType.Password
+        else -> KeyboardType.Text
+    }
+}
+
