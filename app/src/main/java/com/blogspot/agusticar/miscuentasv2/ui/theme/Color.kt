3b1bf1945ba765c4db935ee6484722ca232c8601
Agustin Carnerero Peña <agusticar@gmail.com>
2024-10-09 18:32:36 +0200

    cambio de colores en fondo claro

--- app/src/main/java/com/blogspot/agusticar/miscuentasv2/ui/theme/Color.kt
+++ app/src/main/java/com/blogspot/agusticar/miscuentasv2/ui/theme/Color.kt
@@ -7,6 +7,9 @@ import androidx.compose.ui.graphics.Color
 val DeepPurple = Color(0xFF331436) // Color 1: Un púrpura profundo
 val Crimson = Color(0xFF7A1745) // Color 2: Un carmesí oscuro
 val CoralPink = Color(0xFFCB4757) // Color 3: Un rosa coral
+val DeepBrown = Color(0xFF5C3B28)
+val BurntPeach = Color(0xFFB56438)
+val DarkPeach = Color(0xFFCC7A46)
 val SoftPeach = Color(0xFFEB9961) // Color 4: Un melocotón suave
 val PeachyCream = Color(0xFFFBE0A5) // Un tono suave entre melocotón y amarillo
 val LightYellow = Color(0xFFFCF4B6) // Color 5: Un amarillo claro
