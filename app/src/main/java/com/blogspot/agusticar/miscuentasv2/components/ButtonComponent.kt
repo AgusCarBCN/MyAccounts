676971d9d47c97c6a09e6f17970a1517dfb7c64b
Agustin Carnerero Peña <agusticar@gmail.com>
2024-10-09 18:05:44 +0200

    añado colores animados a los botones

--- app/src/main/java/com/blogspot/agusticar/miscuentasv2/components/ButtonComponent.kt
+++ app/src/main/java/com/blogspot/agusticar/miscuentasv2/components/ButtonComponent.kt
@@ -1,6 +1,10 @@
 package com.blogspot.agusticar.miscuentasv2.components
 
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.padding
@@ -10,8 +14,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
@@ -19,6 +25,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.blogspot.agusticar.miscuentasv2.ui.theme.LocalCustomColorsPalette
+import kotlinx.coroutines.launch
 
 @Composable
 fun ModelButton(
@@ -27,7 +34,41 @@ fun ModelButton(
     modifier: Modifier = Modifier, // Permitir modificar el botón desde el exterior
     enabledButton: Boolean = true,
     onClickButton: () -> Unit
-) {
+) {// Creamos un animatable para manejar el color del ícono
+    val initColorButton = LocalCustomColorsPalette.current.buttonColorDefault
+    val targetColorButton = LocalCustomColorsPalette.current.buttonTransition
+    val initColorText = LocalCustomColorsPalette.current.textButtonColorDefault
+    val targetColorText = LocalCustomColorsPalette.current.textTransition
+
+    val colorButton = remember { Animatable(initColorButton) }
+    val colorText = remember { Animatable(initColorText) }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            // Animación infinita que alterna entre dos colores
+            colorButton.animateTo(
+                targetValue = targetColorButton,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(durationMillis = 5000), // Duración de la transición (1 segundo)
+                    repeatMode = RepeatMode.Reverse // Alterna entre los dos colores
+                )
+            )
+        }
+    }
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            // Animación infinita que alterna entre dos colores
+            colorText.animateTo(
+                targetValue = targetColorText,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(durationMillis = 2000), // Duración de la transición (1 segundo)
+                    repeatMode = RepeatMode.Reverse // Alterna entre los dos colores
+                )
+            )
+        }
+    }
+
     val interactionSource = remember { MutableInteractionSource() }
     val pressed by interactionSource.collectIsPressedAsState()
     // Definir colores
@@ -47,8 +88,8 @@ fun ModelButton(
             .padding(10.dp),
         shape = RoundedCornerShape(10.dp),
         colors = ButtonDefaults.buttonColors(
-            contentColor = if (pressed) pressedContentColor else defaultContentColor,
-            containerColor = if (pressed) pressedContainerColor else defaultContainerColor,
+            contentColor = if (pressed) pressedContentColor else colorText.value,
+            containerColor = if (pressed) pressedContainerColor else colorButton.value,
             disabledContainerColor= disabled
         ),
 
